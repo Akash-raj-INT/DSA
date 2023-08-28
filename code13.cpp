ad655302d3d5e577cdb63209ec51cfa27f4d2a81
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-08-28 14:18:30 +0530

    Add files via upload

---
+++ code13.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void print(int a[], int n)
+{
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
+void bubble(int a[], int n)
+{
+    int i, j, temp;
+    for(i = 0; i < n; i++)
+    {
+        for(j = j+1; j < n; j++)
+        {
+            
+        }
+    }
+}
